f7146d7b00ef0d987177d23bd7a711d51e2f26e9
maliha iqbal <malihaibal47@gmail.com>
2019-04-28 19:44:31 +0500

    first commit

---
+++ djangorest/api/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import CreateView
+
+urlpatterns = {
+    url(r'^bucketlists/$', CreateView.as_view(), name="create"),
+}
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+from .views import DetailsView
+
+url(r'^bucketlists/(?P<pk>[0-9]+)/$',
+        DetailsView.as_view(), name="details"),
